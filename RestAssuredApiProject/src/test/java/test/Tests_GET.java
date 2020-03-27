e72f8f45db5c5a2ab900e7173d6aa53a185a6286
shovalsolo <shovalso@gmail.com>
2020-03-27 09:54:17 -0400

    Adding Test_GET with has items in array

--- RestAssuredApiProject/src/test/java/test/Tests_GET.java
+++ RestAssuredApiProject/src/test/java/test/Tests_GET.java
@@ -10,7 +10,7 @@ package test;
 
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;								//When importing RestAssured in a static way we need to create an object of RestAssured
-
+import static org.hamcrest.Matchers.*;
 
 public class Tests_GET {
 	
@@ -20,6 +20,9 @@ public class Tests_GET {
 		given()
 			.get("https://reqres.in/api/users?page=2")
 		.then()
-			.statusCode(200);
+			.statusCode(200)
+			.body("data.id[1]" ,equalTo(8))								//This will check the cell number 2 has ID of 8 in data array
+			.log().all()												//This will print all the response
+			.body("data.first_name", hasItems("Michael","Lindsay"));	//This will check that data.first_name will have this names
 	}
 }
